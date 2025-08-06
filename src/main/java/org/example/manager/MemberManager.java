a0af9d6dc52d65de6492d488f08a924aea0aa58a
rpsnrpsn <rpsnrpsn@naver.com>
2025-08-06 16:09:26 +0900

    20250806 6교시

--- src/main/java/org/example/manager/MemberManager.java
+++ src/main/java/org/example/manager/MemberManager.java
@@ -4,12 +4,13 @@ import org.example.dao.MemberDao;
 import org.example.db.entity.Member;
 
 import java.sql.SQLException;
+import java.util.List;
 import java.util.Scanner;
 
 public class MemberManager {
     public void run() throws SQLException {
         Scanner sc = new Scanner(System.in);
-        MemberDao memberDao= new MemberDao();
+        MemberDao memberDao = new MemberDao();
 
         while (true) {
             System.out.println("1. 사용자 추가");
@@ -63,23 +64,74 @@ public class MemberManager {
         dao.addMember(newMember);
         System.out.println("사용자가 성공적으로 추가되었습니다.");
     }
+
     // 2. 사용자 전체 조회
     private void viewAllMembers(MemberDao dao) throws SQLException {
+        List<Member> members = dao.findAll();
 
+        if (members.isEmpty()) {
+            System.out.println("등록된 사용자가 없습니다.");
+        } else {
+            members.forEach(System.out::println);
+        }
     }
 
     // 3. 사용자 단건 조회
     private void viewMemberById(Scanner sc, MemberDao dao) throws SQLException {
+        System.out.println("조회할 사용자 ID 입력: ");
+        int id = Integer.parseInt(sc.nextLine());
 
+        Member member = dao.getMemberById(id);
+
+        if (member != null) {
+            System.out.println(member);
+        } else {
+            System.out.println("해당 ID의 사용자를 찾을 수 없습니다.");
+        }
     }
 
     // 4. 사용자 수정
     private void updateMember(Scanner sc, MemberDao dao) throws SQLException {
+        System.out.println("수정할 사용자 ID 입력: ");
+        int id = Integer.parseInt(sc.nextLine());
 
+        Member member = dao.getMemberById(id);
+
+        if (member == null) {
+            System.out.println("해당 ID의 사용자를 찾을 수 없습니다. (수정이 불가합니다)");
+            return;
+        }
+
+        System.out.println("사용자의 새 이름 (변경하지 않으려면 Enter) ");
+        String newName = sc.nextLine();
+
+        System.out.println("사용자의 새 이메일 (변경하지 않으려면 Enter)");
+        String newEmail = sc.nextLine();
+
+        if (!newName.isEmpty()) {
+            member.setName(newName);
+        }
+
+        if (!newEmail.isEmpty()) {
+            member.setEmail(newEmail);
+        }
+
+        if (newName.isEmpty() && newEmail.isEmpty()) {
+            System.out.println("변경 값이 없습니다. (수정이 불가합니다.)");
+            return;
+        }
+
+        dao.updateMember(member);
+        System.out.println("사용자 정보가 성공적으로 업데이트 되었습니다.");
     }
 
     // 5. 사용자 삭제
     private void deleteMember(Scanner sc, MemberDao dao) throws SQLException {
+        System.out.println("삭제할 사용자 ID 입력: ");
+        int id = Integer.parseInt(sc.nextLine());
 
+        dao.deleteMember(id);
+        System.out.println("사용자가 성공적으로 삭제되었습니다.");
     }
 }
+
